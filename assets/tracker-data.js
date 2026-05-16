29c213f733216282e28e5e99a5d17127db80f5de
reveryday <wenswuu@gmail.com>
2026-05-17 01:58:25 +0800

    新增功能

--- assets/tracker-data.js
+++ assets/tracker-data.js
@@ -3,5 +3,7 @@ const TRACKER_CONFIG = {
 };
 
 const TRACKER_ENTRIES = [
-  { date: "2026-05-16", workHours: 4 },
+  { date: "2026-05-14", workHours: 3 },
+  { date: "2026-05-15", workHours: 3.5 },
+  { date: "2026-05-16", workHours: 4.75 },
 ];
